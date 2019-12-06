89740f2bd2b44ef9f973d7f50cbcfde1d4ad6245
Ammar <zarosho@gmail.com>
2019-12-06 14:29:58 +0100

    just added the first file

---
+++ Week2/Playground.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const month ='January';
+
+let season ='';
+
+switch (month){
+    case "January":
+    case "February":
+    season = 'winter'
+    beark; //? 
+    case "April "
+}
+
+console.log(`Season is${cool}`);
+
---
+++ Week2/Temp calc/tempe calc.js
@@ -0,0 +1,31 @@
+//eveytime someone types something we need to take that number and then converted into F and C and show them 
+//all of this should happen right a way. immediately 
+
+const celciusInput = document.querySelector('#celcius > input');
+const fahrenheitInput = document.querySelector('#fahrenheit > input');
+const kelvinInput = document.querySelector('#kelvin > input');
+
+function celciusToFahrenheitAndKelvin(){
+    const cTemp = parseFloat(celciusInput.value);
+    const fTemp = (cTemp * (9/5))+32;
+    const kTemp = cTemp * 273.15;
+    fahrenheitInput.value = fTemp;
+    kelvinInput.value = kTemp;
+}
+function fahrenheitToCelciusAndKelvin (){
+    const fTemp = parseFloat(fahrenheitInput.value);
+    const cTemp = (fTemp - 32)* (5/9);
+    const kTemp = (fTemp + 459.67) * 5/9;
+    celciusInput.value = cTemp;
+    kelvinInput.value = kTemp; 
+}
+function kelvinToCelciusAndFahrenheit (){
+    const kTemp = parseFloat(kelvinInput.value);
+    const cTemp = kTemp - 273.15;
+    const fTemp = 9/5 * (kTemp - 273) +32;
+    celciusInput.value = cTemp;
+    fahrenheitInput.value = fTemp; 
+}
+celciusInput.addEventListener('input', celciusToFahrenheitAndKelvin);
+fahrenheitInput.addEventListener('input',fahrenheitToCelciusAndKelvin);
+kelvinInput.addEventListener('input',kelvinToCelciusAndFahrenheit);
