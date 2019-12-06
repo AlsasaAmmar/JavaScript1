1f3873cbfd1cab7c090413992752ea07ce88fc99
Ammar <zarosho@gmail.com>
2019-12-06 14:51:13 +0100

    dog year is done

---
+++ Week3/Homework/Dog years.js
@@ -0,0 +1,9 @@
+'use strict';
+
+
+const calculateDogAge = age => age * 7;
+
+console.log(calculateDogAge(10));
+console.log(calculateDogAge(2));
+console.log(calculateDogAge(3));
+console.log(calculateDogAge(5));
