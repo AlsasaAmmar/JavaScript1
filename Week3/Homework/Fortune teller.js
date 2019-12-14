dded4e0e2d4991944b4c9eab73e109b1ebe6d25e
AlsasaAmmar <57010390+AlsasaAmmar@users.noreply.github.com>
2019-12-14 21:32:24 +0100

    Update Fortune teller.js

--- Week3/Homework/Fortune teller.js
+++ Week3/Homework/Fortune teller.js
@@ -7,7 +7,7 @@ const partnerNames = ['Fatsonina', 'Jane Austun', 'Scarlett Johansson', 'Beyonc√
 const numChildren = [3 , 15, 1, 0 , 4]
 
 function tellFortune(job, locations, partnerNames, numChildren) { 
-    return `You will be a ${job[Math.floor(Math.random() * 5)]} in ${locations[Math.floor(Math.random() * 5)]}, and married to ${partnerNames[Math.floor(Math.random() * 5)]} with ${numChildren[Math.floor(Math.random() * 5)]} kids.`
+    return `You will be a ${job[Math.floor(Math.random() * jobs.length)]} in ${locations[Math.floor(Math.random() * locations.length)]}, and married to ${partnerNames[Math.floor(Math.random() * partnerNames.length)]} with ${numChildren[Math.floor(Math.random() * numChildren.length)]} kids.`
 }
 
 console.log(tellFortune(jobs, locations, partnerNames, numChildren))
