ba8fa5dea44bc489307ee6af21ecc20aefddc2b8
AlsasaAmmar <57010390+AlsasaAmmar@users.noreply.github.com>
2019-12-14 21:34:00 +0100

    Update Shopping at the supermarket.js
    fixed

--- Week3/Homework/Shopping at the supermarket.js
+++ Week3/Homework/Shopping at the supermarket.js
@@ -6,11 +6,11 @@ const shoppings = ['milk', 'banana' ]
 function addToShoppingCart(item) {
     if (shoppings.length <= 2){
         shoppings.push(item)
-        return console.log(`You bought ${shoppings}!`);
+        console.log(`You bought ${shoppings}!`);
     } else if (shoppings.length <= 3){
         shoppings.shift();
         shoppings.push(item)
-        return console.log(`You bought ${shoppings}!`)
+        console.log(`You bought ${shoppings}!`)
     } 
    
 }
